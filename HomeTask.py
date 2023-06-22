0ddbd47d4adccfb719b84344dbdf7f8066368604
AllaRybak <120407373+AllaRybak@users.noreply.github.com>
2023-06-22 20:19:45 +0300

    Delete HomeTask.py

--- HomeTask.py
+++
@@ -1,6 +0,0 @@
-print('OK')
-print('1 commit - second remote')
-print('2 commit - second remote')
-print('3 commit - second remote')
-print('4 commit - second remote')
-print('5 commit - second remote - by Github')
